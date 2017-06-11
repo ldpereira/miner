2ba042f9c2f5436ff4400a3cec2bd147ce145942
Lucas Dalcol Pereira <lucasdalcolpereira@gmail.com>
2017-06-11 16:17:52 -0300

    ajuste

--- app/js/service.js
+++ app/js/service.js
@@ -4,21 +4,11 @@ var prefix = "http://localhost:8084/MinerWS/service/";
 app.factory('Service', ['$http', function ($http) {
 
     var service = {
-        minerJ48: minerJ48,
-        getContacts: getContacts,
-        sendMessage: sendMessage
+        minerJ48: minerJ48
     };
 
     function minerJ48(file) {
         return $http.get(prefix + 'minerJ48/' + file);
-    }
-
-    function getContacts(group) {
-        return $http.get(prefix + 'getContacts/' + group);
-    }
-
-    function sendMessage(group, message, user) {
-        return $http.get(prefix + 'sendMessage/' + group + "/" + user + "/" + message);
     }
 
     return service;
